14e9e3820e8626fcfbdd1c5abea3ab5b0f334aad
Imre Molnar <imre.molnar.0225@gmail.com>
2021-04-08 08:45:23 +0200

    generation of cards for an example word

--- PeriodicTableWriter/generate.py
+++ PeriodicTableWriter/generate.py
@@ -6,11 +6,20 @@ WORD_LIST_FILE_NAME = 'word_list.txt'
 CANVAS_W, CANVAS_H = 4000, 1000
 CANVAS_BG = 'orange'
 
-ELEMENT_W, ELEMENT_H = 600, 800
-ELEMENT_GAP = 100
+ELEMENT_W, ELEMENT_H = 700, 800
+ELEMENT_GAP = 50
 ELEMENT_BORDER_W = 50
 ELEMENT_BORDER_BG = 'black'
 ELEMENT_BG = 'white'
+
+TEXT_FONT_TYPE = '/usr/share/fonts/freefont/FreeSansBold.ttf'
+TEXT_SIZE_SMALL = 100
+TEXT_SIZE_LARGE = 300
+TEXT_FONT_SMALL = ImageFont.truetype(TEXT_FONT_TYPE, TEXT_SIZE_SMALL)
+TEXT_FONT_LARGE = ImageFont.truetype(TEXT_FONT_TYPE, TEXT_SIZE_LARGE)
+TEXT_MARGIN = 70
+TEXT_PADDING = 10
+TEXT_COLOR = 'black'
 
 ELEMENTS = list()
 WORDS = list()
@@ -21,8 +30,8 @@ def read_elements():
       atomic_number, element, symbol, atomic_mass, _ = line.split(',', 4)
       ELEMENTS.append({
         'atomic_number': atomic_number.strip(),
-        'element': element.strip(),
-        'symbol': symbol.strip().lower(),
+        'name': element.strip(),
+        'symbol': symbol.strip(),
         'atomic_mass': atomic_mass.strip()
       })
   
@@ -40,14 +49,14 @@ def generate_element_word(word):
 
   while word != '':
     start_element = next(
-      (element['symbol'] for element in ELEMENTS
-      if word.startswith(element['symbol'])),
+      (element for element in ELEMENTS
+      if word.startswith(element['symbol'].lower())),
       None
     )
     
     if start_element is not None:
       results.append(start_element)
-      word = word[len(start_element):]
+      word = word[len(start_element['symbol']):]
     else:
       raise Exception('Invalid word: {}'.format(word))
   
@@ -71,14 +80,46 @@ def draw_elements(elements, canvas):
     horizontal_padding = initial_horizontal_padding + index*(ELEMENT_W+ELEMENT_GAP)
     draw_element(horizontal_padding, vertical_padding, element, canvas)
 
-
-def draw_element(start_x, start_y, element, canvas):
-  element_frame = ImageDraw.Draw(canvas)
-  element_frame.rectangle(
+def add_element_frame(start_x, start_y, img):
+  img.rectangle(
     [start_x, start_y, ELEMENT_W + start_x, ELEMENT_H + start_y],
     fill=ELEMENT_BG, outline=ELEMENT_BORDER_BG, width=ELEMENT_BORDER_W
   )
 
+def add_atomic_number(atomic_number, start_x, start_y, img):
+  target_x = start_x + TEXT_MARGIN
+  target_y = start_y + TEXT_MARGIN
+  img.text((target_x, target_y), atomic_number, TEXT_COLOR, TEXT_FONT_SMALL)
+
+def add_symbol(symbol, start_x, start_y, img):
+  small_text_w, small_text_h = img.textsize(symbol, TEXT_FONT_SMALL)
+  text_w, text_h = img.textsize(symbol, TEXT_FONT_LARGE)
+  target_x = start_x + (ELEMENT_W-text_w)/2
+  target_y = start_y + TEXT_MARGIN + small_text_h + TEXT_PADDING + (ELEMENT_H - 2*TEXT_MARGIN - 3*TEXT_PADDING - 3*small_text_h - text_h)/2
+  
+  img.text((target_x, target_y), symbol, TEXT_COLOR, TEXT_FONT_LARGE)
+
+def add_name(name, start_x, start_y, img):
+  text_w, text_h = img.textsize(name, TEXT_FONT_SMALL)
+  target_x = start_x + (ELEMENT_W-text_w)/2
+  target_y = start_y + ELEMENT_H - TEXT_MARGIN - 2*text_h - TEXT_PADDING
+  img.text((target_x, target_y), name, TEXT_COLOR, TEXT_FONT_SMALL)
+
+def add_atomic_mass(atomic_mass, start_x, start_y, img):
+  text_w, text_h = img.textsize(atomic_mass, TEXT_FONT_SMALL)
+  target_x = start_x + (ELEMENT_W-text_w)/2
+  target_y = start_y + ELEMENT_H - TEXT_MARGIN - text_h
+  img.text((target_x, target_y), atomic_mass, TEXT_COLOR, TEXT_FONT_SMALL)
+
+def draw_element(start_x, start_y, element, canvas):
+  element_img = ImageDraw.Draw(canvas)
+  add_element_frame(start_x, start_y, element_img)
+  print(element)  
+  add_atomic_number(element['atomic_number'], start_x, start_y, element_img)
+  add_symbol(element['symbol'], start_x, start_y, element_img)
+  add_name(element['name'], start_x, start_y, element_img)
+  add_atomic_mass(element['atomic_mass'], start_x, start_y, element_img)
+  
 
 def generate_image(elements):
   canvas = create_canvas()
